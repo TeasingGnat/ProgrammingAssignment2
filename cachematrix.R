e5654eef6bdc02b0f060518e129170f3d5fd4d1d
TeasingGnat <thomas.c.carr@gmail.com>
2015-06-21 20:37:35 +0100

    updated again with addition explanation

--- cachematrix.R
+++ cachematrix.R
@@ -7,14 +7,14 @@
 ## the inverse when calculated
 
 makeCacheMatrix <- function(x = matrix()) {
-  m <- NULL
-  set <- function (y) {
+  m <- NULL  ## sets default for m as NULL
+  set <- function (y) { ## opens the function for set that is specific to this function
     x <<- y
     m <<- NULL
   }
-  get <-function() x
-  setmatrix <- function(solve) m<<- solve
-  getmatrix <- function() m
+  get <-function() x ## 
+  setmatrix <- function(solve) m<<- solve ## inverts the matrix
+  getmatrix <- function() m ## gets the information stored
   list (set=set, get=get, setmatrix=setmatrix, getmatrix=getmatrix)
 }
 
@@ -26,17 +26,18 @@ makeCacheMatrix <- function(x = matrix()) {
 ## it then prints the matrix
 
 cacheSolve <- function(x, ...) {
-  m <- x$getmatrix()
-  if(!is.null(m)){
+  m <- x$getmatrix() ## sets m as the getmatrix
+  if(!is.null(m)){ ## checks whether there is data in there if yes fetches it
     message("getting cached data")
     return(m)
   }
-  data <- x$get()
+  data <- x$get() ## if ther is no data above calculates it and stores it then prints it.
   m <- solve(data,...)
   x$setmatrix(m)
   m
   ## Return a matrix that is the inverse of 'x'
 }
+## this additional code just shows that the above has worked.
 n <- matrix(1:4,2,2)
 h <- makeCacheMatrix(n)
 cacheSolve(h)
